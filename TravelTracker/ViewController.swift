64ede3ea1e9af6abc0156f42750b6292f09ef048
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2019-03-25 08:08:14 -0600

    WhirlyGlobe Files and basic geography layer info

--- TravelTracker/ViewController.swift
+++ TravelTracker/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import WhirlyGlobeMaplyComponent
 
 class ViewController: UIViewController {
+    
+    private var maplyVC: MaplyBaseViewController? //myViewC
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        maplyVC = WhirlyGlobeViewController()
+//        view.addSubview(maplyVC!.view)
+//        maplyVC!.view.frame = view.bounds
+//        addChild(maplyVC!)
+//        
+//        let globeVC = maplyVC as? WhirlyGlobeViewController
+//
+//        // we want a black background for a globe, a white background for a map.
+//        maplyVC!.clearColor = (globeVC != nil) ? UIColor.black : UIColor.white
+//
+//        maplyVC!.frameInterval = 2
+//
+//        // set up the data source
+//        if let tileSource = MaplyMBTileSource(mbTiles: "geography-class_medres"),
+//            let layer = MaplyQuadImageTilesLayer(tileSource: tileSource) {
+//            layer.handleEdges = (globeVC != nil)
+//            layer.coverPoles = (globeVC != nil)
+//            layer.requireElev = false
+//            layer.waitLoad = false
+//            layer.drawPriority = 0
+//            layer.singleLevelLoading = false
+//            maplyVC!.add(layer)
+//        }
+//
+//        globeVC!.height = 0.8
+//        globeVC!.animate(toPosition: MaplyCoordinateMakeWithDegrees(-3.6704803, 40.5023056), time: 1.0)
+    
     }
 
 
