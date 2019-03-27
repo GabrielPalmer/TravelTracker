d43555a84b0401956b461043d694283b108f4273
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2019-03-27 08:59:40 -0600

    fixed segue identifier

--- TravelTracker/Controllers/MapViewController.swift
+++ TravelTracker/Controllers/MapViewController.swift
@@ -16,32 +16,32 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        maplyVC = WhirlyGlobeViewController()
-//        view.addSubview(maplyVC!.view)
-//        maplyVC!.view.frame = view.bounds
-//        addChild(maplyVC!)
-//
-//        let globeVC = maplyVC as? WhirlyGlobeViewController
-//
-//        // we want a black background for a globe, a white background for a map.
-//        maplyVC!.clearColor = (globeVC != nil) ? UIColor.black : UIColor.white
-//
-//        maplyVC!.frameInterval = 2
-//
-//        // set up the data source
-//        if let tileSource = MaplyMBTileSource(mbTiles: "geography-class_medres"),
-//            let layer = MaplyQuadImageTilesLayer(tileSource: tileSource) {
-//            layer.handleEdges = (globeVC != nil)
-//            layer.coverPoles = (globeVC != nil)
-//            layer.requireElev = false
-//            layer.waitLoad = false
-//            layer.drawPriority = 0
-//            layer.singleLevelLoading = false
-//            maplyVC!.add(layer)
-//        }
-//
-//        globeVC!.height = 0.8
-//        globeVC!.animate(toPosition: MaplyCoordinateMakeWithDegrees(-3.6704803, 40.5023056), time: 1.0)
+        maplyVC = WhirlyGlobeViewController()
+        view.addSubview(maplyVC!.view)
+        maplyVC!.view.frame = view.bounds
+        addChild(maplyVC!)
+
+        let globeVC = maplyVC as? WhirlyGlobeViewController
+
+        // we want a black background for a globe, a white background for a map.
+        maplyVC!.clearColor = (globeVC != nil) ? UIColor.black : UIColor.white
+
+        maplyVC!.frameInterval = 2
+
+        // set up the data source
+        if let tileSource = MaplyMBTileSource(mbTiles: "geography-class_medres"),
+            let layer = MaplyQuadImageTilesLayer(tileSource: tileSource) {
+            layer.handleEdges = (globeVC != nil)
+            layer.coverPoles = (globeVC != nil)
+            layer.requireElev = false
+            layer.waitLoad = false
+            layer.drawPriority = 0
+            layer.singleLevelLoading = false
+            maplyVC!.add(layer)
+        }
+
+        globeVC!.height = 0.8
+        globeVC!.animate(toPosition: MaplyCoordinateMakeWithDegrees(-3.6704803, 40.5023056), time: 1.0)
     
         
     }
