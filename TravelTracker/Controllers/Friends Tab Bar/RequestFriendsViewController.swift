3f27f940bc6a2eacede8b0e2b798626feb9363eb
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2019-05-02 07:36:23 -0600

    changed view controller names Setup UI for requesting friend but not implemented

---
+++ TravelTracker/Controllers/Friends Tab Bar/RequestFriendsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  TravelTracker
+//
+//  Created by Gabriel Blaine Palmer on 3/26/19.
+//  Copyright © 2019 Gabriel Blaine Palmer. All rights reserved.
+//
+
+import UIKit
+
+class RequestFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
